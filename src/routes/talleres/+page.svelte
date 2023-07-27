1cf3d946c6ae2c232ae5f87ebd46782e0fb84ea5
Chef <cheftech@patraldo.dev>
2023-07-27 17:08:18 -0600

    changed talleres link to /exhibiciones

--- src/routes/talleres/+page.svelte
+++ src/routes/talleres/+page.svelte
@@ -1,5 +1,5 @@
 <div class='taller'>
-<p style="font-size:5vw;">Taller abierto: dibujos animados con inteligencia artificial. Gratis, en el espacio del <a href="https://plantas.patraldo.com">¡Plantas Patraldo!</a></p> 
+<p style="font-size:5vw;">Taller abierto: dibujos animados con inteligencia artificial. Gratis, en el espacio del <a href="/exhibiciones">La Musa que Mas Aplauda</a></p> 
 </div>
 
 <style>
