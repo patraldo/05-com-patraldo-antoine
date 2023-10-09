d6b59b3d5093f50f7380151982e96e1790125457
Chef Tech <cheftech@patraldo.com>
2023-10-09 11:14:11 -0600

    changed color gradient

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { Body } from 'svelte-body';
     const style = {
-        color: 'white',
+        color: 'green',
         '--cool-css-prop': '😎'
     };
 
@@ -15,27 +15,12 @@
 
 <Body {style} />
 
-<!-- <p style="font-size:5vw;">{path}Acerca de ANTOINEX</p> -->
-
-<!-- <div class='bienvenido'> 
-  <h1 style="font-size:9vw">Bienvenid@ a mi galería en linea:</h1>
-</div>
--->
-
 <div class='musx'> 
   <h1 style="font-size:11vw">La Musa que Más Aplauda</h1>
 </div>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Bungee+Spice&family=Pacifico&family=Special+Elite&family=Tangerine:wght@400;700&family=Ultra&display=swap');
 
-@font-palette-values --colorized {
-      font-family: "Bungee Spice";
-      base-palette: 0;
-/*    override-colors: 1 hotpink, 2 cyan, 2 white; */
-/*    override-colors: 0 #00ffbb, 1 #007744; */
-      override-colors: 0 DarkGreen, 1 Blue;
-
-}
 
 @keyframes move-bg {
   to {
@@ -46,15 +31,13 @@
 /* https://web.dev/speedy-css-tip-animated-gradient-text/ */
 .musx {
   --bg-size: 400%;
-  --color-one: hsl(15 90% 55%);
-  --color-two: hsl(50 55% 15%);
-/*  border: 10px dotted black; */
+  --color-one: hsl(5 10% 35%);
+  --color-two: hsl(90 95% 35%);
   background-clip: padding-box;
 font-family: 'Special Elite';
 margin: 10px;
 padding: 10px;
 text-align: center;
-/*background: #ddd392;*/
 
 background: linear-gradient(
                 90deg,
